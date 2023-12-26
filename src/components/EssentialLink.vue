ff76dc12c10cc6adc2e65e1164f32ba9b92f981e
soroushrahmani <soro.rahi@gmail.com>
2023-12-26 16:08:53 +0330

    First commt

---
+++ src/components/EssentialLink.vue
@@ -0,0 +1,12 @@
+<template>
+  hi
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'EssentialLink',
+
+})
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- Your component's template goes here -->
+  </div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+
+export default {
+  name: 'MyComponent',
+  setup() {
+    onMounted(() => {
+      window.location.href = 'http://www.barainvest.com'
+    })
+  }
+}
+</script>
